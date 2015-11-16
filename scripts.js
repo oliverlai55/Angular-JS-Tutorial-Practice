e32b30aa419225b11ed645331f1ad84d344bc6b5
oliverlai55 <oliverlai55@gmail.com>
2015-11-16 00:58:28 -0500

    practicing using angular JS

---
+++ scripts.js
@@ -0,0 +1,34 @@
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+scotchApp.controller('maincontroller', function($scope){
+
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			});
+		});
+	$scope.message = "Everyone come and see how good I look!";
+})；
+
+scotchApp.controller('aboutController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+});
